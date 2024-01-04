141dbab19aa7fdd37ebea1822c18b8a32974214b
guntarakesh <guntarakesh971@gmail.com>
2024-01-04 20:33:27 +0530

    authenicated the mongodb

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1/inotebook?directConnection=true"
+const mongoURI = "mongodb+srv://guntarakesh7:A3offdPoemkicjih@cluster0.oxayffk.mongodb.net/?retryWrites=true&w=majority"
 
 const connectToMongo = async()=>{
-   try{
-    mongoose.set("strictQuery", false);
-    mongoose.connect(mongoURI);
-    console.log("Connected to mongodb sucessfully");
+   try {
+      mongoose.set("strictQuery", false);
+      await mongoose.connect(mongoURI, {
+         useNewUrlParser: true,
+         useUnifiedTopology: true
+      });
+      console.log("Connected to MongoDB successfully");
    }
    catch(error){
     console.log(error);
